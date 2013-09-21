d939506bc803f42b447f1c05ce23fbb87ab260f3
Scott Brown <brown131@yahoo.com>
2013-09-21 00:17:28 -0500

    broke out metadata

--- keywords.rkt
+++ keywords.rkt
@@ -1,8 +1,14 @@
 #lang racket
-  
+;;;; Racquel - An ORM for Racket
+;;;;
+;;;; keywords - Defines syntactic keywords
+;;;;
+;;;; Copyright (c) Scott Brown 2013
+
 (provide table-name column external-name primary-key)
 
 (define-syntax table-name (syntax-rules ()))
 (define-syntax column (syntax-rules ()))
 (define-syntax external-name (syntax-rules ()))
 (define-syntax primary-key (syntax-rules ()))
+(define-syntax join (syntax-rules ()))
--- test.rkt
+++ test.rkt
@@ -1,40 +1,60 @@
 #lang racket
-
+;;;; Racquel - An ORM for Racket
+;;;;
+;;;; test - Tests the project
+;;;;
+;;;; Copyright (c) Scott Brown 2013
 (require rackunit rackunit/text-ui db racquel)
 
-;(data-class2 (table-name "test") (column (name #f "id")))
-;(data-class2 (table-name "test") (external-name "Test"))
-
-(require/expose racquel (data-class-metadata%
-                         savable-fields 
+(require/expose racquel (savable-fields 
                          primary-key-where-clause 
                          insert-sql 
                          update-sql 
                          delete-sql 
-                         select-sql))
+                         select-sql
+                         object-class))
 
+(require/expose "metadata.rkt" (data-class-metadata% *data-class-metadata*))
+  
 ;;;; SETUP
  
 ;;; Test database connection
 (define con (mysql-connect #:server "localhost" #:port 3306 #:database "racquel_test" #:user "test" #:password "test"))
 
-;;; Test object class
-(define test-object% (data-class "test" '("id" "name" "description") #:primary-key "id"))
-
 ;;;; TESTS
-
+#|
+(data-class data-object%
+            (table-name "test")
+            (column [id #f "id"] [name #f "name"] [description #f "description"])
+            (primary-key "id")
+            (inspect #f)
+            (super-new)
+            )
+|#
 (define-test-suite test-define-data-object
- (let* ([obj (new test-object%)])
-   (test-case "test object created?" (check-not-eq? test-object% #f))
+ (let* ([test-class% (data-class data-object%
+                                 (table-name "test")
+                                 (column [id #f "id"] [name #f "name"] [description #f "description"])
+                                 (primary-key "id")
+                                 (inspect #f)
+                                 (super-new)
+                                 )]
+        [obj (new test-class%)])
+   (test-case "test class created?" (check-not-eq? test-class% #f))
+   (test-true "test class is a data class?" (data-class? test-class%))
+   (test-case "test object created?" (check-not-eq? obj #f))
    
    (test-case "data object metadata set?" 
-              (let-values ([(tbl-nm col-nms pkey auto-key ext-nm cls-nm) (data-class-info test-object%)])
+              (let-values ([(tbl-nm col-nms pkey auto-key ext-nm cls-nm) (data-class-info test-class%)])
                 (check-eq? tbl-nm "test")
                 (check-equal? col-nms '("id" "name" "description"))
                 (check-eq? pkey "id")
                 (check-eq? auto-key #f)
                 (check-eq? ext-nm "test")
-                (check-eq? cls-nm 'test%)))
+                (check-eq? cls-nm 'test-class%)
+                ))
+   
+   (test-case "object class correct?" (check-equal? (object-class obj) test-class%))
    
    (test-case "fields set?" 
               (set-field! id obj 1)
@@ -44,24 +64,48 @@
               (check-eq? (get-field name obj) "Test")
               (check-eq? (get-field description obj) "This is a test"))
    
-   (test-case "savable field correct?" (check-equal? (savable-fields con obj) '("name" "description")))
-   (test-case "where clause correct?" (check-equal? (primary-key-where-clause con obj) " where id=?"))
-   (test-case "insert sql correct?" (check-equal? (insert-sql con obj) "insert test (name, description) values (?, ?)"))
-   (test-case "update sql correct?" (check-equal? (update-sql con obj) "update test set name=?, description=? where id=?"))
-   (test-case "delete sql correct?" (check-equal? (delete-sql con obj) "delete from test where id=?"))
+   (test-case "savable field correct?" (check-equal? (savable-fields con test-class%) '("id" "name" "description")))
+   (test-case "where clause correct?" (check-equal? (primary-key-where-clause con test-class%) " where id=?"))
+   (test-case "insert sql correct?" (check-equal? (insert-sql con test-class%) "insert test (id, name, description) values (?, ?, ?)"))
+   (test-case "update sql correct?" (check-equal? (update-sql con test-class%) "update test set id=?, name=?, description=? where id=?"))
+   (test-case "delete sql correct?" (check-equal? (delete-sql con test-class%) "delete from test where id=?"))
    (test-case "select sql correct?" 
-              (check-equal? (select-sql con obj "where id=?") "select id, name, description from test t where id=?"))
+              (check-equal? (select-sql con test-class% "where id=?") "select id, name, description from test t where id=?"))
    )
 )
 
 (define-test-suite test-make-data-object
  (let* ([simple% (gen-data-class con "simple")]
         [obj (new simple%)])
+   (test-case "simple class created?" (check-not-eq? simple% #f))
+   (test-true "simple class is a data class?" (data-class? simple%))
    (test-case "simple object created?" (check-not-eq? obj #f))
+   
+   (test-case "simple class metadata set?" 
+              (let-values ([(tbl-nm col-nms pkey auto-key ext-nm cls-nm) (data-class-info simple%)])
+                (check-eq? tbl-nm "simple")
+                (check-equal? col-nms '("name" "description" "id"))
+                (check-eq? pkey "id")
+                (check-eq? auto-key #f)
+                (check-eq? ext-nm "simple")
+                (check-eq? cls-nm 'simple%)
+                ))
+   
+   (test-case "object class correct?" (check-equal? (object-class obj) simple%))
+   
+   (test-case "savable field correct?" (check-equal? (savable-fields con simple%) '("name" "description" "id")))
+   (test-case "where clause correct?" (check-equal? (primary-key-where-clause con simple%) " where id=?"))
+   (test-case "insert sql correct?" (check-equal? (insert-sql con simple%) "insert simple (name, description, id) values (?, ?, ?)"))
+   (test-case "update sql correct?" (check-equal? (update-sql con simple%) "update simple set name=?, description=?, id=? where id=?"))
+   (test-case "delete sql correct?" (check-equal? (delete-sql con simple%) "delete from simple where id=?"))
+   (test-case "select sql correct?" 
+              (check-equal? (select-sql con simple% "where id=?") "select name, description, id from simple t where id=?"))
   
    (test-case "fields set?"
+              (set-field! id obj 23)
               (set-field! name obj "test")
               (set-field! description obj "this is a test")
+              (check-eq? (get-field id obj) 23)
               (check-eq? (get-field name obj) "test")
               (check-eq? (get-field description obj) "this is a test"))
    
@@ -86,7 +130,31 @@
               (check-eq? (query-value con "select count(*) from simple where id=?" (get-field id obj)) 0)) 
    ))
 
+#|
+(define ds (data-class data-object% 
+             (table-name "test") 
+             (external-name "Test") 
+             (column (name #f "name") (color #f "color"))
+             (field (x 1) (y 2))
+             (super-new)
+             (inspect #f)))
+ds
+|#
+
+(define-test-suite test-data-class
+ (let* ([cls (data-class data-object% 
+             (table-name "test") 
+             (external-name "Test") 
+             (column (name #f "name") (color #f "color"))
+             (field (x 1) (y 2))
+             (super-new)
+             (inspect #f))])
+   (test-true "data class parsed?" (class? cls))
+  ; cls
+))
+
 (run-tests test-define-data-object 'verbose)
 (run-tests test-make-data-object 'verbose)
-
+(run-tests test-data-class 'verbose)
+  
 (disconnect con)
