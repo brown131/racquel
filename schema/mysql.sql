879cd548542ba7d3b4ce4e8e1487ba7d5fc924b7
Scott Brown <brown131@yahoo.com>
2013-09-21 00:17:28 -0500

    broke out metadata

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -1,7 +1,7 @@
 /* Test schema for MySQL */
 
 create table simple (
-  id int(11) not null auto_increment,
+  id int(11) not null,
   `name` varchar(30) default null,
   description varchar(500) default null,
   primary key (id)
@@ -17,8 +17,8 @@ create table auto (
 create table if not exists multipartKey (
   simple_id int(11) not null,
   auto_id int(11) not null,
-  primary key (simple_id, exponent_id),
+  primary key (simple_id, auto_id),
   key k_simple_id_multipartKey (simple_id),
   constraint fk_simple_id_multipartKey foreign key (simple_id) references simple (id),
-  constraint fk_auto_id_languageexponent foreign key (auto_id) references auto (id)
+  constraint fk_auto_id_multipartKey foreign key (auto_id) references auto (id)
 ) engine=InnoDB default charset=latin1;
